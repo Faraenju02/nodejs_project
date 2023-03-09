e502700a1d600a436c482662561242d8b3ed0d0a
Faraenju02 <fabianracini@hotmail.com>
2023-03-09 14:18:12 -0500

    my _first_website_nodejs

---
+++ routers/index.js
@@ -0,0 +1,15 @@
+import {Router} from "express";
+
+const router = Router();
+
+router.get("/", (req, res) => res.render("index",{title:"First website with nodejs"}));
+
+router.get("/about", (req, res) => res.render("about",{title:"About Me"}));
+
+router.get("/contact", (req, res) => res.render("contact",{title:"contact"}));
+
+
+router.get("/help", (req, res) => res.render("help"));
+
+export default router
+
